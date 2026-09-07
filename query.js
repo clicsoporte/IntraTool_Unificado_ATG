252296fd6b438dc4f236b42f0a6a5f29065e38e0
clicsoporte <68799572+clicsoporte@users.noreply.github.com>
2026-09-07 09:27:24 -0600

    chore: actualizar .gitignore y remover binarios y evidencias temporales de git

--- query.js
+++
@@ -1,5 +0,0 @@
-const db = require('better-sqlite3')('dbs/clic_tools.db');
-const res = db.prepare(`
-  SELECT FACTURA FROM core_erp_invoice_headers WHERE FACTURA LIKE '%4705'
-`).all();
-console.log(res);
